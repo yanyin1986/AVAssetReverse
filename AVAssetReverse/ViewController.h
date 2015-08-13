c6c9676ae4cdba41cf2fdb821718c0c51fb66ddf
Leon.yan <leonyan@LeonyandeMacBook-Pro.local>
2015-08-13 23:04:17 +0800

    Initial Commit

---
+++ AVAssetReverse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVAssetReverse
+//
+//  Created by Leon.yan on 8/13/15.
+//  Copyright (c) 2015 Leon.yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AVAssetReverse/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AVAssetReverse
+//
+//  Created by Leon.yan on 8/13/15.
+//  Copyright (c) 2015 Leon.yan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
