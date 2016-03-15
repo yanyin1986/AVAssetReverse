d1e22c7fbb9cad0bbd3d3ee4a8b624b3b4cf6aa1
Leon.yan <yanyin1986@gmail.com>
2016-03-15 15:49:36 +0800

    update demo

--- AVAssetReverse/ViewController.m
+++ AVAssetReverse/ViewController.m
@@ -12,7 +12,14 @@
 #import "AVAssetReverseSession.h"
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UIView *playerView;
+
+@property (nonatomic, weak  ) IBOutlet UIButton *reverseButton;
+@property (nonatomic, weak  ) IBOutlet UIButton *cancelButton;
+@property (nonatomic, weak  ) IBOutlet UIProgressView *progressView;
+@property (nonatomic, weak  ) IBOutlet UILabel *infoLabel;
+
+@property (nonatomic, strong) AVAssetReverseSession *reverseSession;
+@property (nonatomic, strong) NSTimer *reverseTimer;
 
 @end
 
@@ -39,5 +46,65 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)reverse:(id)sender
+{
+    if (!_reverseSession) {
+        NSURL *fileURL = [[NSBundle mainBundle] URLForResource:@"IMG_2246" withExtension:@"MOV"];
+        AVURLAsset *asset = [AVURLAsset assetWithURL:fileURL];
+        AVAssetReverseSession *session = [[AVAssetReverseSession alloc] initWithAsset:asset];
+        NSString *outputURL = [NSTemporaryDirectory() stringByAppendingPathComponent:@"output.mp4"];
+        if ([[NSFileManager defaultManager] fileExistsAtPath:outputURL]) {
+            [[NSFileManager defaultManager] removeItemAtPath:outputURL error:nil];
+        }
+        session.outputFileType = AVFileTypeMPEG4;
+        session.outputURL = [NSURL fileURLWithPath:outputURL];
+        NSDate *startTime = [NSDate new];
+        NSTimer *checkTimer = [NSTimer
+                               scheduledTimerWithTimeInterval:1.0 / 10.0
+                               target:self
+                               selector:@selector(checkStatus:)
+                               userInfo:nil
+                               repeats:YES];
+        _reverseTimer = checkTimer;
+        [session reverseAsynchronouslyWithCompletionHandler:^{
+            if (session.status == AVAssetReverseSessionStatusCompleted) {
+                NSLog(@"finished");
+            } else {
+                NSLog(@"rever failed");
+            }
+            [self finishedWithTime:(-[startTime timeIntervalSinceNow])];
+        }];
+        
+        _reverseSession = session;
+        _cancelButton.enabled = YES;
+    }
+}
+
+- (IBAction)cancel:(id)sender
+{
+    if (_reverseSession) {
+        _cancelButton.enabled = NO;
+        [_reverseSession cancelReverse];
+    }
+}
+
+- (void)checkStatus:(id)sender
+{
+    if (_reverseSession) {
+        _progressView.progress = _reverseSession.progress;
+    }
+}
+
+- (void)finishedWithTime:(NSTimeInterval)time
+{
+    NSString *message = [NSString stringWithFormat:@"time spent: %g", time];
+    _infoLabel.text = message;
+    _reverseSession = nil;
+    [_reverseTimer invalidate];
+    _reverseTimer = nil;
+    _cancelButton.enabled = NO;
+}
+
 
 @end
